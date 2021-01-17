75c325eb4557e42cf1dd2d22f2eaa58433bb807e
papgergo1997 <pap.gergo.1997@gmail.com>
2021-01-17 12:17:31 +0100

    scss-project-footer-mobile-design-complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
+let modal = document.querySelector('.section__modal');
+let modalBtn = document.querySelector('.modal__button');
+let closeBtn = document.querySelector('.close');
+let closeBtn2 = document.querySelector('.close2');
+
+
 $(document).ready(function () {
     let scrollLink = $('.scroll');
     scrollLink.click(function (e) {
@@ -40,3 +46,21 @@ $(function () {
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
+
+modalBtn.onclick = function () {
+    modal.style.display = "block";
+    modal.classList.add("showModal");
+};
+
+closeBtn.onclick = function () {
+    modal.style.display = "none";
+};
+closeBtn2.onclick = function () {
+    modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
